af7235a746243389fb1a9e43dc73151e4e3fbd64
rupali21 <rupali21.mitech@gmail.com>
2024-01-04 17:46:38 +0530

    file added

---
+++ blog.php
@@ -0,0 +1,24 @@
+<section class="blog">
+    <div class="container-fluid ">
+        <div class="sec-title">
+          <h2 class="section-title1 px-5" style="padding-top:25px";><span class="px1-2"><b>Our Blog</b></span></h2>
+        </div>
+        <div class="row px-xl-5 pt-4">
+            <div class="col-lg-6 table-responsive mb-5">
+                <h6>What is Lorem Ipsum?</h6>
+                <p style="text-align: justify;font-size: 12px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div>
+                <button  class="btn btn-primary text " >
+          View More
+         </button>
+     </div>
+            </div>
+            <div class="col-lg-4">
+                <div style="padding-left: 158px;">
+                    <iframe width="280" height="150" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    
